91aaa4fb4fa9c4075374c696525aa649a64810b2
Chaicoo <gabriellimma27@gmail.com>
2023-11-22 21:51:14 -0300

    feat: create layout from screen rooms

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,32 +1,41 @@
 import { prismaClient } from "@/lib/prisma";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
+import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, BookCopyIcon, BookOpenIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Ranking from "./components/ranking";
 
 const Room = async ({ params }: any) => {
   const materialsRoom = [
     {
       id: 1,
-      title: "Material 1",
-      description: "Descrição do material 1",
+      description: "Atividade avaliativa referente a N2",
       date: new Date,
       type: "activity",
     },
     {
       id: 2,
-      title: "Material 2",
-      description: "Descrição do material 2",
+      description: "Material apresentado em sala referente aos assuntos da etapa N2",
       date: new Date,
       type: "content",
     },
-    { id: 3,
-      title: "Material 3",
-      description: "Descrição do material 3",
+    {
+      id: 3,
+      description: "Slides apresentados em sala",
       date: new Date,
       type: "content",
     }
+  ];
+
+  const coments = [
+    {
+      id: 1,
+      date: new Date,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis a cras semper auctor neque vitae tempus quam. Venenatis cras sed felis eget velit aliquet sagittis id. Velit ut tortor pretium viverra suspendisse. Neque viverra justo nec ultrices dui sapien eget mi proin. A condimentum vitae sapien pellentesque habitant morbi.",
+      idUser: 1,
+    },
   ];
 
   const room = await prismaClient.room.findUnique({
@@ -50,29 +59,72 @@ const Room = async ({ params }: any) => {
 
   return (
     <>
-      <Card className="flex flex-col items-center justify-between p-[1.87rem] mt-4 h-40 ml-8 mr-8">
-        <CardHeader className="text-center">
+      <Badge variant="outline" className="py-3 flex justify-end items-center gap-7 rounded-sm p-[1.87rem] mt-4 max-h-full ml-8 mr-8">
+        <CardHeader className="text-center mx-auto">
           <CardTitle>{room.name}</CardTitle>
           <CardDescription>Professor {professor?.name}</CardDescription>
-          <CardDescription><p>Código: {room.code}</p></CardDescription>
+          <CardDescription ><p>Código: {room.code}</p></CardDescription>
         </CardHeader>
-        <CardContent>
-        </CardContent>
-      </Card>
+      </Badge>
+
       <div className="flex flex-row w-full max-w-sm items-center gap-1.5 mt-4 m-auto">
         <Input type="text" id="coment" placeholder="Digite um comentário para a turma" />
         <Button variant="outline" size="icon">
           <ChevronRightIcon className="h-4 w-4" />
         </Button>
       </div>
+
+      <div className="mt-8 mx-auto flex  flex-col items-center justify-center">
+        <Ranking />
+      </div>
+      {
+        coments.map((coment) => (
+          <Badge key={coment.id} variant="outline" className="flex flex-col rounded-sm justify-between mt-6 ml-12 mr-12">
+            <div className="flex flex-row justify-between ">
+              <CardHeader className="text-left">
+                <Avatar>
+                  <AvatarFallback>
+                  </AvatarFallback>
+
+                  {professor?.image && (
+                    <AvatarImage src={professor?.image} alt="profile image" />
+                  )}
+                </Avatar>
+                <CardDescription>{professor?.name} comentou:</CardDescription>
+                <CardDescription>{coment.description}</CardDescription>
+              </CardHeader>
+              <CardFooter>
+                <CardDescription>Postado em: {new Date(coment.date).toLocaleDateString()}</CardDescription>
+              </CardFooter>
+            </div>
+            <div className="flex flex-row w-full max-w-sm items-center gap-1.5 mt-2 m-auto mb-2">
+              <Input type="text" id="coment" placeholder="Responder" />
+              <Button variant="outline" size="icon">
+                <ChevronRightIcon className="h-4 w-4" />
+              </Button>
+            </div>
+          </Badge>
+        ))
+      }
       {
         materialsRoom.map((material) => (
-          <Card key={material.id} className="flex flex-col justify-between mt-6 ml-12 mr-12 h-24">
+          <Card key={material.id} className="flex justify-between mt-6 ml-12 mr-12">
             <CardHeader className="text-left">
-              <CardTitle>{material.title}</CardTitle>
+              <CardTitle className="flex flex-row gap-4">
+                {material.type === 'activity' && (
+                  <BookOpenIcon className="h-6 w-6" />
+                )}
+                {material.type === 'content' && (
+                  <BookCopyIcon className="h-6 w-6" />
+                )}
+                {professor?.name} postou {
+                  material.type === "activity" ? "um atividade" : " um conteúdo"
+                }
+              </CardTitle>
               <CardDescription>{material.description}</CardDescription>
             </CardHeader>
             <CardFooter>
+              <CardDescription>Postado em: {new Date(material.date).toLocaleDateString()}</CardDescription>
             </CardFooter>
           </Card>
         ))
